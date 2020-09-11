203108325ee1990d447559b4f6d0842a17e85453
MedEBoudali <med.sneaky@gmail.com>
2020-09-11 18:25:24 +0000

    add jest and scripts

---
+++ App.js
@@ -0,0 +1,12 @@
+const googleDatabase = [
+    'cats.com',
+    'dogs.com',
+    'catsfood.com',
+    'ilovecats',
+    'animals.com',
+    'pets.com'
+];
+
+const googlSearch = (searchIput, DB) => {
+    const matches = 
+}
