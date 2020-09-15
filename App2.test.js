936b4bb75a67783be6ed98fd22dcd251b87eae4b
MedEBoudali <med.sneaky@gmail.com>
2020-09-15 23:02:51 +0000

    add async tests

--- App2.test.js
+++ App2.test.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const swapi = require('./App2');
 
-// test promise to use asyn text you need to edit timeout: 30000
+// test promise to use async/await you need to edit timeout: 30000
 // 1st way: add done so we wait the promise to finish then pass to the next one and add timeout
 it('calls swapi to get people with done', done => {
   // how many assertions we expect, to test them all before go to the next
@@ -31,12 +31,15 @@ it('calls swapi to get people with return', () => {
   });
 }, 30000);
 
+// 3rd way using asyn/await
 it('calls swapi to get people with async/await', async () => {
   // how many assertions expected
   expect.assertions(3);
-  const data = await swapi.getPeopleAsync(fetch);
-  //assertion
-  expect(data.funcType).toEqual('Async/Await');
-  expect(data.count).toEqual(20);
-  expect(data.results).toEqual('Async/Await');
+  const { funcType, count, results } = await swapi.getPeopleAsync(fetch);
+  //1st assertion
+  expect(funcType).toEqual('Async/Await');
+  //2nd assertion
+  expect(count).toBeGreaterThan(5);
+  //3rd assertion
+  expect(results.length).toBeGreaterThan(5);
 }, 30000);
