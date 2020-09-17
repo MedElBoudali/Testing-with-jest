377bb1556063bccfe8dda82e5f20f09d5fd326bb
MedEBoudali <med.sneaky@gmail.com>
2020-09-17 16:28:34 +0000

    moving to components testing

--- App2.test.js
+++ App2.test.js
@@ -39,7 +39,29 @@ it('calls swapi to get people with async/await', async () => {
   //1st assertion
   expect(funcType).toEqual('Async/Await');
   //2nd assertion
-  expect(count).toBeGreaterThan(4);
+  expect(count).toBeGreaterThan(5);
   //3rd assertion
-  expect(results.length).toBeGreaterThan(4);
-}, 30000);
+  expect(results.length).toBeGreaterThan(5);
+}, 40000);
+
+// Mocks: fake functions and pretand running, it let us spy on the behavior of a function that is called indirectly by some other call instead of waiting
+it('getPeople returns funcType count and results', () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          funcType: 'AsyncAwait',
+          count: 82,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+  expect.assertions(5);
+  return swapi.getPeopleAsync(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+    expect(data.funcType).toEqual('Async/Await');
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
